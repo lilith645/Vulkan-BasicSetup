47e214c477fceabc149743fb15ab91e1a3fcaea2
Lilith645 <lilith@inet-sys.com>
2017-05-13 09:21:10 +1000

    Colour overlay on textures now work, Updated .gitignore to ignore .spv files Changes committed: 	modified:   .gitignore 	modified:   Display.h 	modified:   main.cpp 	modified:   shaders/shader.frag

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <vulkan/vulkan.h>
 #include <iostream>
 #include <stdexcept>
